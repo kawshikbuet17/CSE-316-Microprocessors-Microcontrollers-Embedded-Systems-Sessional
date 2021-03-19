7c198b7fdabf4e932231c4d8c12977d8e10a3bb4
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2021-03-19 14:46:10 +0600

    added comments

--- Offline-03-Procedure-Loop/1705043.asm
+++ Offline-03-Procedure-Loop/1705043.asm
@@ -23,21 +23,24 @@ MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
     
+    ; num1 input message
     LEA DX, input1_msg
     MOV AH, 9
     INT 21H
 
+    ; num1 input as decimal
     CALL INDEC 
-
-    PUSH AX
-    MOV num1, AX
-      
     
+    ; push num1 into stack
+    PUSH AX
+    MOV num1, AX ; take num1 backup
+      
+    ; operator input message
     LEA DX, operator_msg
     MOV AH, 9
     INT 21H
     
-    ;take operator
+    ;take operator input
     MOV AH, 1
     INT 21H
     
@@ -61,18 +64,18 @@ MAIN PROC
     JMP WRONG_OPERATOR
     
     ADDITION:
-        POP AX
-        MOV BX, AX
+        POP AX                  ;get num1
+        MOV BX, AX              ;store num1 into BX
         
-        LEA DX, input2_msg
+        LEA DX, input2_msg      ;num2 input message
         MOV AH, 9
         INT 21H
         
         CALL INDEC
      
-        ADD AX, BX
+        ADD AX, BX              ;num1+num2
         
-        MOV ans, AX
+        MOV ans, AX             ;ans = num1+num2
         
         
         LEA DX, output_msg
@@ -174,116 +177,122 @@ MAIN PROC
     
 MAIN ENDP
 
+
+;DECIMAL output procedure
 INDEC PROC
-    
     PUSH BX
     PUSH CX
     PUSH DX 
     
 @BEGIN:
-    XOR BX, BX
+    XOR BX, BX          ;clear BX
     
-    XOR CX, CX
+    XOR CX, CX          ;clear CX
     
-    MOV AH, 1
+    MOV AH, 1           ;take char input
     INT 21H
     
-    CMP AL, '-'
+    CMP AL, '-'         ;handle minus sign
     JE @MINUS
     
-    CMP AL, '+'
+    CMP AL, '+'         ;handle plus sign
     JE @PLUS
-    JMP @REPEAT2
+    
+    JMP @REPEAT2        ;handle no sign (consider as plus)
+
 @MINUS:
-    MOV CX, 1
+    MOV CX, 1           ;CX=1 is minus sign
+
 @PLUS:
-    INT 21H
+    INT 21H             ;take next char input
     
 @REPEAT2:
-    CMP AL, '0'
-    JNGE @AGAIN
+    CMP AL, '0'         
+    JNGE @AGAIN         ;if char<0 go to @AGAIN
 
-    CMP AL, '9'
-    JNLE @AGAIN
+    CMP AL, '9'         
+    JNLE @AGAIN         ;if char>9 go to @AGAIN
     
     
-    AND AX, 000FH
-    PUSH AX
+    AND AX, 000FH       ;ASCII to DECIMAL and store to AX
+    PUSH AX             ;push DECIMAL digit to stack
     
-    MOV AX, 10
-    MUL BX
-    POP BX
-    ADD BX, AX
+    MOV AX, 10          
+    MUL BX              
+    POP BX              
+    ADD BX, AX          ;BX = total*10 + decimal 
     
     @AGAIN:
     
     MOV AH, 1
     INT 21H
     CMP AL, 0DH
-    JNE @REPEAT2
+    JNE @REPEAT2        ;if char is not \n, repeat input
+                        ;else
+    MOV AX, BX          ;store ans in AX
     
-    MOV AX, BX
+    OR CX, CX           ;if CX==0
+    JE @EXIT            ;done, go to @EXIT
     
-    OR CX, CX
-    JE @EXIT
-    
-    NEG AX
+    NEG AX              ;make 2's complement is CX != 0
     
 @EXIT:
     POP DX
     POP CX
     POP BX
-    RET
-
+    RET                 ;return
 INDEC ENDP
-
-
+ 
+ 
+;DECIMAL output procedure
 OUTDEC PROC
+    ;push values of registers
     PUSH AX
     PUSH BX
     PUSH CX
     PUSH DX
     
-    OR AX, AX
-    JGE @END_IF1
-    
-    PUSH AX
-    MOV DL, '-'
+    OR AX, AX           
+    JGE @END_IF1        ;if AX>=0 go to @END_IF1
+                        ;else
+    PUSH AX             
+    MOV DL, '-'         ;print '-' sign
     MOV AH, 2
     INT 21H
     POP AX
-    NEG AX
+    NEG AX              ;make 2's completement
     
 @END_IF1:
-    XOR CX, CX
-    MOV BX, 10D
+    XOR CX, CX          ;clear CX
+    MOV BX, 10D         ;BX = 10
     
 @REPEAT1:
-    XOR DX, DX
-    DIV BX
-    PUSH DX
-    INC CX
+    XOR DX, DX          ;clear DX
+    DIV BX              ;DX = AX % 10 , AX = AX / 10
+    PUSH DX             ;push remainer into stack
+    INC CX              ;count digit++
     
-    OR AX, AX
-    JNE @REPEAT1
-    
+    OR AX, AX           
+    JNE @REPEAT1        ;if AX!=0, repeat
+                        ;else
     MOV AH, 2
 
 @PRINT_LOOP:
-    POP DX
-    OR DL, 30H
-    INT 21H
-    LOOP @PRINT_LOOP
+    POP DX              ;pop MSB of DECIMAL
+    OR DL, 30H          ;DECIMAL to ASCII
+    INT 21H             ;print
+    LOOP @PRINT_LOOP    ;till CX=0, @PRINT_LOOP continues
     
+    ;pop all in reverse order of push
     POP DX
     POP CX
     POP BX
-    POP AX
-    
-    RET
+    POP AX                  
+    RET                 ;return
 OUTDEC ENDP
 
 
+;new line procedure
 \N PROC
         MOV AH, 2
         MOV DL, 0DH

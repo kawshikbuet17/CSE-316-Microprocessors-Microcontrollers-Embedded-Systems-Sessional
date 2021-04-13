beb788b6fc1d33afce25db7575e4abfb1b135088
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2021-04-13 16:14:53 +0600

    circuit changed

--- Offline-05-ATmega32-LED-Matrix/LED-Matrix/LED-Matrix-Code/main.cpp
+++ Offline-05-ATmega32-LED-Matrix/LED-Matrix/LED-Matrix-Code/main.cpp
@@ -8,37 +8,45 @@
 #define F_CPU 1000000
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+volatile unsigned int pattern_P[8] = { 0b11111111, 0b00000000, 0b00000000, 0b00100111, 0b00100111, 0b00000111, 0b00000111, 0b11111111};
+
+ISR(INT0_vect){
+	PORTC = ~PORTC;
+}
 
 void baseState(void){
-	PORTD = 0b10000000;
+	PORTA = 0b10000000;
 	PORTB = 0b11111111;
 	_delay_ms(.5);
 	
-	PORTD = 0b01000000;
+	PORTA = 0b01000000;
 	PORTB = 0b00000000;
 	_delay_ms(.5);
 	
-	PORTD = 0b00100000;
+	PORTA = 0b00100000;
 	PORTB = 0b00000000;
 	_delay_ms(.5);
 	
-	PORTD = 0b00010000;
+	PORTA = 0b00010000;
 	PORTB = 0b00100111;
 	_delay_ms(.5);
 	
-	PORTD = 0b00001000;
+	PORTA = 0b00001000;
 	PORTB = 0b00100111;
 	_delay_ms(.5);
 	
-	PORTD = 0b00000100;
+	PORTA = 0b00000100;
 	PORTB = 0b00000111;
 	_delay_ms(.5);
 	
-	PORTD = 0b00000010;
+	PORTA = 0b00000010;
 	PORTB = 0b00000111;
 	_delay_ms(.5);
 	
-	PORTD = 0b00000001;
+	PORTA = 0b00000001;
 	PORTB = 0b11111111;
 	_delay_ms(.5);
 }
@@ -47,10 +55,14 @@ int main(void)
 {
     DDRD = 0xFF; //column output
     DDRB = 0xFF; //row output
-
+	PORTC = 0b00000001;
+	GICR = (1<<INT0); //STEP3
+	MCUCR = MCUCR & 0b11110011;//STEP4
     while (1) 
     {
-		baseState();		
+		baseState();
+		//PORTA = PORTA>>1;	
+		//_delay_ms(100);	
     }
 }
 
